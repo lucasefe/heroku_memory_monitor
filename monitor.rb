03eac172a5f504b859824c59c74069b478426600
Lucas Florio <lucasefe@gmail.com>
2013-10-03 10:21:58 -0300

    Better, simpler way to process the logs.

--- monitor.rb
+++ monitor.rb
@@ -4,48 +4,44 @@ require 'heroku-api'
 require 'open-uri'
 
 STDOUT.sync = true
-app_name = 'credible-chefsfeed'
 
-puts "[monitor] Starting to monitor #{app_name}"
+APP_NAME = ENV['HEROKU_APP_NAME']
+API_KEY  = ENV['HEROKU_API_KEY']
+MEMORY_CODE = "R14"
+
+raise "Provide HEROKU_API_KEY" if API_KEY.nil?
+
+puts "[monitor] Starting to monitor #{APP_NAME}"
 
 begin
-  API_KEY = ENV['HEROKU_API_KEY']
-  raise "Provide HEROKU_API_KEY" if API_KEY.nil?
-  MAX_DYNO_SIZE=500
-  CODE = "R14"
 
   heroku = Heroku::API.new(api_key: API_KEY)
-
-  # make sure this range will cover your web dynos.
-  dynos = (1..20).to_a
-  dyno_types = %w(web worker)
-
   previous_timestamps = Hash.new(0)
   restarts = Hash.new
 
   while true
     begin
       puts "[monitor] Fetching log"
-      response = open(heroku.get_logs(app_name, 'num' => '1000').body).readlines.reverse.select{|l| l.include?(CODE) }
+      response = open(heroku.get_logs(APP_NAME, 'num' => '1000').body).readlines.reverse.select{|l| l.include?(MEMORY_CODE) }
     rescue => ex
       puts "[monitor] Unable to fetch log (#{ex.message})"
     end
 
     if response
-      dyno_types.each do |type|
-        dynos.each do |i|
-          dyno_name = "#{type}.#{i}"
-          restarts[dyno_name] ||= Time.now
-          dyno_response = response.detect{|l| l.include? "[#{dyno_name}]" }
-          if dyno_response
-      if (Time.now - restarts[dyno_name]) > 600
-              restarts[dyno_name] = Time.now
-              puts "[monitor] RESTARTING #{dyno_name} based on #{CODE}"
-              puts heroku.post_ps_restart(app_name, "ps" => dyno_name)
-      else
-          puts "[monitor] grace period for #{dyno_name}"
-            end
-    end
+
+      response.each do |dyno_response|
+        dyno_match= dyno_response.match(/(web|worker)\.(\d*)/)
+        dyno_name = "#{dyno_match[1]}.#{dyno_match[2]}"
+        restarts[dyno_name] ||= Time.now
+
+        if dyno_response
+            if (Time.now - restarts[dyno_name]) > 600
+            restarts[dyno_name] = Time.now
+            puts "[monitor] RESTARTING #{dyno_name} based on #{MEMORY_CODE}"
+            puts heroku.post_ps_restart(APP_NAME, "ps" => dyno_name)
+          else
+            puts "[monitor] grace period for #{dyno_name}"
+          end
         end
       end
     else
